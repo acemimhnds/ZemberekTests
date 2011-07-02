16e0db373e1b4d3e7088d9f2e383e97f5829f854
Merve ÇAVDAR <mhndsmerve@hotmail.com>
2011-07-02 15:19:14 +0300

    Kullanıcının yapacağı işlemi seçebilmesi özelliği katıldı

--- src/ZemberekTests.java
+++ src/ZemberekTests.java
@@ -1,4 +1,3 @@
-
 import net.zemberek.erisim.Zemberek;
 import net.zemberek.tr.yapi.TurkiyeTurkcesi;
 import net.zemberek.yapi.Kelime;
@@ -85,6 +84,7 @@ public class ZemberekTests {
     public static void dosyadanOku() {
         Zemberek z = new Zemberek(new TurkiyeTurkcesi());
         Scanner n = new Scanner(System.in);
+        Scanner q = new Scanner(System.in);
         BufferedReader oku;
         String adres;
         System.out.println("Adresi giriniz:");
@@ -103,30 +103,78 @@ public class ZemberekTests {
         }
         String dizi2[] = null;
         dizi2 = s.split(" ");
+        System.out.println("Uygulanacak islemi secip numarasini giriniz:");
+        System.out
+                .print("1.Hecele\n2.Asciya donustur\n3.Kelime denetle\n4.Kelime cozumle\n5.Kelime ayristir\n6.Oneriler\n7.Hepsi :)");
+        int l;
+        l = q.nextInt();
         System.out.println("İslem yapilacak  cumle:" + s);
-        heceler(s, dizi2, z);
-        asciDonustur(s, z);
-        oneriler(dizi2, z);
-        kelimeDenetle(dizi2, z);
-        kelimeCozumle(dizi2, z);
-        kelimeAyristir(dizi2, z);
+        if (l == 1) {
+            heceler(s, dizi2, z);
+        }
+        if (l == 2) {
+            asciDonustur(s, z);
+        }
+        if (l == 3) {
+            kelimeDenetle(dizi2, z);
+        }
+        if (l == 4) {
+            kelimeCozumle(dizi2, z);
+        }
+        if (l == 5) {
+            kelimeAyristir(dizi2, z);
+        } else if (l == 6) {
+            oneriler(dizi2, z);
+        } else if (l == 7) {
+            heceler(s, dizi2, z);
+            asciDonustur(s, z);
+            oneriler(dizi2, z);
+            kelimeDenetle(dizi2, z);
+            kelimeCozumle(dizi2, z);
+            kelimeAyristir(dizi2, z);
+        }
+        
     }
 
     public static void kullanicidanAl() {
         Zemberek z = new Zemberek(new TurkiyeTurkcesi());
         Scanner m = new Scanner(System.in);
+        Scanner v = new Scanner(System.in);
         String k;
         System.out.println("lutfen cumleyi giriniz:");
         k = m.nextLine();
         String dizi[] = null;
         dizi = k.split(" ");
+        System.out.println("Uygulanacak islemi secip numarasnı giriniz:");
+        System.out
+                .print("1.Hecele\n2.Asciya donustur\n3.Kelime denetle\n4.Kelime cozumle\n5.Kelime ayristir\n6.Oneriler\n7.Hepsi :)");
+        int l;
+        l = v.nextInt();
         System.out.println("İslem yapilacak  cumle:" + k);
-        heceler(k, dizi, z);
-        asciDonustur(k, z);
-        oneriler(dizi, z);
-        kelimeDenetle(dizi, z);
-        kelimeCozumle(dizi, z);
-        kelimeAyristir(dizi, z);
+        if (l == 1) {
+            heceler(k, dizi, z);
+        }
+        if (l == 2) {
+            asciDonustur(k, z);
+        }
+        if (l == 3) {
+            kelimeDenetle(dizi, z);
+        }
+        if (l == 4) {
+            kelimeCozumle(dizi, z);
+        }
+        if (l == 5) {
+            kelimeAyristir(dizi, z);
+        } else if (l == 6) {
+            oneriler(dizi, z);
+        } else if (l == 7) {
+            heceler(k, dizi, z);
+            asciDonustur(k, z);
+            oneriler(dizi, z);
+            kelimeDenetle(dizi, z);
+            kelimeCozumle(dizi, z);
+            kelimeAyristir(dizi, z);
+        }
 
     }
 
@@ -135,10 +183,11 @@ public class ZemberekTests {
         System.out
                 .println("Islem yapilacak cumleyi kullanicidan almak icin 1'i ,dosyadan okutmak icin 2'yi tuslayiniz.");
         Scanner x = new Scanner(System.in);
-       int girdi = x.nextInt();
-        if (girdi==2) {
+        int girdi = x.nextInt();
+        if (girdi == 2) {
             dosyadanOku();
-        } else if(girdi==1)  {
+
+        } else if (girdi == 1) {
             kullanicidanAl();
 
         }
